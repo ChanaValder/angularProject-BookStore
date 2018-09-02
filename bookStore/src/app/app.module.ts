3b3e32f9e1d9720d0fb21b05685f8bd412bc5035
chayawaxstock <c0556777462@gmail.com>
2018-09-02 09:42:29 +0300

    ok

--- bookStore/src/app/app.module.ts
+++ bookStore/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProductDitailsComponent } from './components/product-ditails/product-ditails.component';
+
 import { AccountComponent } from './components/account/account.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component:HomeComponent  },
+ 
+];
+
 
 @NgModule({
   declarations: [
@@ -20,14 +26,12 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     MainComponent,
     ProductsComponent,
     HomeComponent,
-    ProductDitailsComponent,
     AccountComponent,
     CartComponent,
     ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,  RouterModule.forRoot( appRoutes )],
   providers: [],
   bootstrap: [AppComponent]
 })
