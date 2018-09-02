7bb68f260883f239d5d489e0cf3ff7031b0773ca
chayawaxstock <c0556777462@gmail.com>
2018-09-02 09:43:33 +0300

    eddf

--- bookStore/src/app/app.module.ts
+++ bookStore/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProductDitailsComponent } from './components/product-ditails/product-ditails.component';
+
 import { AccountComponent } from './components/account/account.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -20,7 +19,6 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     MainComponent,
     ProductsComponent,
     HomeComponent,
-    ProductDitailsComponent,
     AccountComponent,
     CartComponent,
     ProductDetailsComponent
