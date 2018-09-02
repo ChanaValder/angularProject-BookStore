db947dda51bff9c4600e99858d527b195e7db127
chayawaxstock <c0556777462@gmail.com>
2018-09-02 12:04:36 +0300

    pp

--- bookStore/src/app/app.module.ts
+++ bookStore/src/app/app.module.ts
@@ -14,15 +14,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import{BookStoreService} from '././shared/services/book-store.service'
 import { registerContentQuery } from '../../node_modules/@angular/core/src/render3/instructions';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
-<<<<<<< HEAD
   { path: 'bookStore/home', component:HomeComponent  },
-  { path: 'bookStore/home/login', component:LoginComponent  },
-  { path: 'bookStore/home/register', component:RegisterComponent  },
-  { path: 'bookStore/home/login', component:RegisterComponent  },
+
   { path: 'bookStore/myAccount', component:AccountComponent  },
-=======
   { path: '', component:HomeComponent },
    { path: 'bookStore', component:HomeComponent },
   
@@ -31,7 +28,6 @@ const appRoutes: Routes = [
 },
   { path: 'bookStore/myAccount/login', component: LoginComponent }, // url: about/
   { path: 'bookStore/myAccount/register', component: RegisterComponent } ,// url: about/item
->>>>>>> 084f560139bc8acd762cad4d2895b82f38c75062
   { path: 'bookStore/products', component:ProductsComponent  },
  { path: 'bookStore/myCart', component:CartComponent  },
 
@@ -53,7 +49,11 @@ const appRoutes: Routes = [
     RegisterComponent
   ],
   imports: [
-    BrowserModule,  RouterModule.forRoot( appRoutes )],
+    BrowserModule, 
+     RouterModule.forRoot( appRoutes ),
+     ReactiveFormsModule, 
+    FormsModule,],
+
   providers: [BookStoreService],
   bootstrap: [AppComponent]
 })
--- bookStore/src/app/components/login/login.component.ts
+++ bookStore/src/app/components/login/login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
 
-  ngOnInit() {
+  //----------------PROPERTIRS-------------------
+  formGroup: FormGroup;
+  obj: typeof Object = Object;
+
+  //----------------CONSTRUCTOR------------------
+  constructor() {
+    let formGroupConfig = {
+      userName: new FormControl("", this.createValidatorArr("name", 3, 15,/^[A-Za-z]+$/)),
+      userPassword: new FormControl("", this.createValidatorArr("password", 5, 10))
+    };
+
+    this.formGroup = new FormGroup(formGroupConfig);
   }
 
+  //----------------METHODS-------------------
+  submitLogin() {
+   
+  }
+
+
+  createValidatorArr(cntName: string, min: number, max: number,pattern?:RegExp): Array<ValidatorFn> {
+    return [
+      f => !f.value ? { "val": `${cntName} is required` } : null,
+      f =>pattern&&f.value && !f.value.match(pattern)  ? { "val": `${cntName} is contains only English letters` } : null,
+      f => f.value && f.value.length > max ? { "val": `${cntName} is max ${max} chars` } : null,
+      f => f.value && f.value.length < min ? { "val": `${cntName} is min ${min} chars` } : null
+     
+    ];
+  }
 }
