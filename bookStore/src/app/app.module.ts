3117c53ca82aac1778dff05b88ad1f1699d1cd3b
chayawaxstock <c0556777462@gmail.com>
2018-09-02 09:52:33 +0300

    sss

--- bookStore/src/app/app.module.ts
+++ bookStore/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-=======
-import { RouterModule, Routes } from '@angular/router';
->>>>>>> 3b3e32f9e1d9720d0fb21b05685f8bd412bc5035
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { ProductsComponent } from './components/products/products.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './components/account/account.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
