524db0a81af8596ee96c987f005c698391265c91
chayawaxstock <c0556777462@gmail.com>
2018-09-02 13:05:20 +0300

    dd

--- bookStore/src/app/shared/services/book-store.service.ts
+++ bookStore/src/app/shared/services/book-store.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import{User} from '../models/User.model'
 import { Adress } from '../models/Adress.model';
-<<<<<<< HEAD
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { error } from '../../../../node_modules/protractor';
-=======
-import { HttpClient } from '@angular/common/http';
 
->>>>>>> bb5697a204588b3c8992ddc9ca07a5eaff948997
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,25 +13,19 @@ import { HttpClient } from '@angular/common/http';
 export class BookStoreService {
 adressStore:Adress
   user:User;
-<<<<<<< HEAD
-  basicUrlLogin="http://localhost:3500/api/login";
-  constructor(private myHttpClient: HttpClient) {
-=======
-  basicURL:string="http://localhost:3500/api"
+
+  basicURL="http://localhost:3500/api"
   constructor(public httpClient:HttpClient) {
->>>>>>> bb5697a204588b3c8992ddc9ca07a5eaff948997
     this.user=null;
     this.adressStore=new Adress("Hirsh",15,"Bnei-Brak");
    }
 
-<<<<<<< HEAD
    login(user): void {
-    this.myHttpClient.get(this.basicUrlLogin,user).subscribe(
+    this.httpClient.get(this.basicURL+"/login",user).subscribe(
         (res) => { alert("good")},err=>{alert("bad")}
         
     )
 }
-=======
 
    registerUser(newUser:User): void {
     let url: string = this.basicURL + "/register";
@@ -46,6 +37,5 @@ adressStore:Adress
         alert("error");
     });
   }
->>>>>>> bb5697a204588b3c8992ddc9ca07a5eaff948997
  
 }
