6a2fda8d052b451e6eddf8e49aafd1a30e39b8f2
chayawaxstock <c0556777462@gmail.com>
2018-09-03 10:21:58 +0300

    ooo

--- bookStore/src/app/components/cart/cart.component.ts
+++ bookStore/src/app/components/cart/cart.component.ts
@@ -7,30 +7,18 @@ import { VolumeInfo } from '../../shared/models/volum-info.model';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-<<<<<<< HEAD
   myCartBook:VolumeInfo[];
-=======
-  myCartBook: VolumeInfo[];
->>>>>>> 01c98132ab98cf5c08fb3650244b03f61c246175
 
   constructor(public bookService: BookStoreService) { }
 
   ngOnInit() {
-
     this.bookService.subject.subscribe(
       {
         next: (v: any) => {
           this.myCartBook = v;
         }
       })
-<<<<<<< HEAD
     this.myCartBook= this.bookService.getMyCart();
-=======
-    this.myCartBook = this.bookService.getMyCart();
->>>>>>> 01c98132ab98cf5c08fb3650244b03f61c246175
-
-
-
   }
 
 }
