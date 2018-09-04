7eb87b02c1b63403d33d5844d0a2d1218e1831b6
chayawaxstock <42530885+chayawaxstock@users.noreply.github.com>
2018-09-05 01:19:57 +0300

    sss

--- bookStore/src/app/components/products/products.component.ts
+++ bookStore/src/app/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../shared/models/book.model';
 import { BookStoreService } from '../../shared/services/book-store.service';
-import { VolumeInfo } from '../../shared/models/volum-info.model';
 
 @Component({
   selector: 'app-products',
@@ -10,18 +9,21 @@ import { VolumeInfo } from '../../shared/models/volum-info.model';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor(private bookStoreService: BookStoreService) { }
   bookList: Book[];
+  constructor(private bookStoreService: BookStoreService) { }
+
   ngOnInit() {
-   if(this.bookStoreService.statusSearch==1)
-   {
-      document.getElementById("search").setAttribute("value",this.bookStoreService.search)
+
+    //if click on the button go back to list i want with the search
+    if (this.bookStoreService.statusSearch == 1) {
+      document.getElementById("search").setAttribute("value", this.bookStoreService.search)
       this.bookStoreService.getBooks(this.bookStoreService.search).subscribe(res => { this.bookList = res["items"]; }, err => { });
-    } 
-    else this.bookStoreService.getBooks().subscribe(res => { this.bookList = res["items"]; }, err => { });
-   this.bookStoreService.statusSearch=0;
+    }
+    else
+      this.bookStoreService.getBooks().subscribe(res => { this.bookList = res["items"]; }, err => { });
+    this.bookStoreService.statusSearch = 0;
   }
-  
+
   search(keySearch) {
     this.bookStoreService.getBooks(keySearch).subscribe(res => { this.bookList = res["items"]; }, err => { });
   }
